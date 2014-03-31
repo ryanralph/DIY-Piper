1f28a5d21ba61074743ffbc1584c81b57e47fdcd
Ryan Ralph <ryan@ryanralph.net>
2014-03-31 15:13:59 +0000

    Added runner.py

---
+++ runner.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import os
+import time
+
+printButton = 24
+remember = 25
+printing = False
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(remember, GPIO.IN)
+GPIO.setup(printButton, GPIO.IN)
+
+while 1:
+	if (GPIO.input(printButton) == True):
+		if GPIO.input(remember) == False:
+			print("Forget")
+			os.system("sudo python printkeys.py -f 1")
+			time.sleep(10)
+		else:
+			print("Remember")
+			os.system("sudo python printkeys.py -r 1")
+			time.sleep(10)
+	else:
+		print("switch not pressed")
