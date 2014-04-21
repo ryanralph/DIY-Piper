5fbeb25d00c3d811c0112d386d8bfbc40c1acf47
Ryan Ralph <ryan@ryanralph.net>
2014-04-21 21:38:41 +1000

    First rev of quickinstall ready for testing

---
+++ quickinstall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt-get update
+apt-get install git python-pip python-dev build-essential python-serial libpcre3 libpcre3-dev
+pip install --upgrade pip
+pip install --upgrade virtualenv
+pip install pillow qrcode pycrypto scrypt slowaes ecdsa
+sed -i '$ d' /etc/inittab
+sed -e "s/console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 //g" -i.backup /boot/cmdline.txt
+git clone https://github.com/adafruit/Python-Thermal-Printer.git
+git clone https://github.com/ryanralph/Piper.git
+git clone https://github.com/piperwallet/vanitygen.git
+cd vanitygen/
+make
+mv vanitygen ~/Piper/
+cd ~/Piper
+
