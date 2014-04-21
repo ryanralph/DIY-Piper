31310645044141f27a9129f145fc1b01f51799a6
Ryan Ralph <ryan@ryanralph.net>
2014-04-22 00:34:58 +1000

    Runner.py

--- runner.py
+++ runner.py
@@ -2,22 +2,26 @@ import RPi.GPIO as GPIO
 import os
 import time
 
-printButton = 24
-remember = 25
+printButton = 25
+remember = 7
+dataLED = 8
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(remember, GPIO.IN)
 GPIO.setup(printButton, GPIO.IN)
+GPIO.setup(dataLED, GPIO.OUTPUT)
 
 while 1:
 	if (GPIO.input(printButton) == True):
 		if GPIO.input(remember) == False:
-#			print("Forget")
-			os.system("sudo python printkeys.py -f 1")
+			print("Forget")
+			os.system("sudo ./Piper/printkeys.py -f 1")
 			time.sleep(10)
 		else:
-#			print("Remember")
-			os.system("sudo python printkeys.py -r 1")
+			print("Remember")
+			os.system("sudo ./Piper/ printkeys.py -r 1")
 			time.sleep(10)
-#	else:
+		GPIO.output(dataLED, False)
+	else:
 #		print("switch not pressed")
+		GPIO.output(dataLED, True)
